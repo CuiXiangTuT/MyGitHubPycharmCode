9b3e73994e92f3cb3cc1f4c9a82a39f9d7d68552
cuixiang0613 <cuixiang0613@163.com>
2024-03-28 16:01:07 +0800

    MyTest Code First Commit

---
+++ MyTest/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2024/3/28 16:00
+@Author  : HuTao
+@File    : __init__.py
+@Description :
+"""
---
+++ MyTest/my_test_001.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2024/3/28 16:00
+@Author  : HuTao
+@File    : my_test_001
+@Description :
+"""
+print("这是一次测试")
