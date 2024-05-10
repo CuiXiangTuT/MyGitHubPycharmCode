8c90962eda8885aae96ec976c1f718b4500f1706
cuixiang0613 <cuixiang0613@163.com>
2024-05-11 00:49:44 +0800

    second submit

---
+++ MyTest/my_test_002.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2024/5/11 0:39
+@Author  : HuTao
+@File    : my_test_002
+@Description :
+"""
+print("这是第二次测试")
+print("Hello World")
---
+++ MyTest/my_test_003.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2024/5/11 0:48
+@Author  : HuTao
+@File    : my_test_003
+@Description :
+"""
+print("Hello My Test !")
